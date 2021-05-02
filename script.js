13bfa64b0c7001a4624a31a1ff94f18a539397d0
Adam Landriault <>
2021-05-02 19:57:17 +0000

    fixed errors again

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
- document.getElementById('button').addEventListener('click', myFunction)
- function myFunction() {
-  alert ('Hi Mr. Di')
-  document.getElementById('button').style.fontSize ='20px'
-  document.getElementById('button').style.color ='red'
+document.getElementById('button').addEventListener('click', myFunction)
+function myFunction () {
+  alert('Hi Mr. Di')
+  document.getElementById('button').style.fontSize = '20px'
+  document.getElementById('button').style.color = 'red'
   document.getElementById('button').style.backgroundColor = 'blue'
 }
 document.write('Adam Landriault')
